c03a894b88526db1768f3fabbb1463bad7d80997
Любовь Беркова <grandprinceess7777@gmail.com>
2022-02-18 23:30:22 +0300

    комментарий

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,9 +5,9 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title
@@ -24,4 +24,4 @@ class Post(models.Model):
         Group,
         on_delete=models.CASCADE,
         related_name='posts',
-        blank=True, null=True, max_length=100)
+        blank=True, null=True, max_length=200)
