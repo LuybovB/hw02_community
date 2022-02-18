01000b894077a9872be105400dc7a65075b8e769
Любовь Беркова <grandprinceess7777@gmail.com>
2022-02-18 23:13:52 +0300

    комментарий

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
+
 
 from .models import Post
 from .models import Group
-# Главная страница
+
+
 def index(request):
     template = 'posts/index.html'
     posts = Post.objects.order_by('-pub_date')[:10]
@@ -16,6 +17,7 @@ def index(request):
     }
     return render(request, template, context)
 
+
 def group_posts(request, slug):
     template = 'posts/group_posts.html'
     group = get_object_or_404(Group, slug=slug)
@@ -26,17 +28,17 @@ def group_posts(request, slug):
     }
     return render(request, template, context)
 
+
 def group_list(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     title = 'Страница сообщества'
     text = 'Лев Толстой – зеркало русской революции.'
-
     context = {
         'group': group,
         'posts': posts,
         'title': title,
         'text': text,
-    }    
+    }
     return render(request, template, context)
