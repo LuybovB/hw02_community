f5bf6c3ad0c32e067682685afcb1cd13dfade9f9
Любовь Беркова <grandprinceess7777@gmail.com>
2022-02-20 22:18:15 +0300

    комментарий

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -9,11 +9,7 @@ LIMIT_POST = 10
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.select_related(
-        'group'
-    ).all().order_by(
-        '-pub_date'
-    )[:LIMIT_POST]
+    posts = Post.objects.select_related('group').all()[:LIMIT_POST]
     context = {
         'posts': posts,
     }
