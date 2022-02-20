9d99d883f100ea76013f02fa3dd4ff2b0d5d2568
Любовь Беркова <grandprinceess7777@gmail.com>
2022-02-20 10:54:34 +0300

    комментарий

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -9,7 +9,7 @@ LIMIT_POST = 10
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[LIMIT_POST]
+    posts = Post.objects.order_by('-pub_date')[:LIMIT_POST]
     text = 'Последние обновления на сайте'
     context = {
         'posts': posts,
@@ -21,7 +21,7 @@ def index(request):
 def group_list(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[LIMIT_POST]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:LIMIT_POST]
     text = 'Лев Толстой – зеркало русской революции.'
     context = {
         'group': group,
