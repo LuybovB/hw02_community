1830aca28523f626617fa661a1c2f8d2aac6de7e
Любовь Беркова <grandprinceess7777@gmail.com>
2022-02-26 19:10:17 +0300

    комментарий

---
+++ yatube/users/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+# users/views.py
+# Импортируем CreateView, чтобы создать ему наследника
+from django.views.generic import CreateView
+
+# Функция reverse_lazy позволяет получить URL по параметрам функции path()
+# Берём, тоже пригодится
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+
+# Импортируем класс формы, чтобы сослаться на неё во view-классе
+from .forms import CreationForm
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    # После успешной регистрации перенаправляем пользователя на главную.
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
+
+def authorized_only(func):
+    # Функция-обёртка в декораторе может быть названа как угодно
+    def check_user(request, *args, **kwargs):
+        # В любую view-функции первым аргументом передаётся объект request,
+        # в котором есть булева переменная is_authenticated,
+        # определяющая, авторизован ли пользователь.
+        if request.user.is_authenticated:
+            # Возвращает view-функцию, если пользователь авторизован.
+            return func(request, *args, **kwargs)
+        # Если пользователь не авторизован — отправим его на страницу логина.
+        return redirect('/auth/login/')        
+    return check_user
+
+
+# Декорируем view-функцию
+#@authorized_only
+#def some_view(request):
