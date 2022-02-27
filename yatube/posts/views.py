f0ac94437df084e67be3b28f2c9f921535263066
Любовь Беркова <grandprinceess7777@gmail.com>
2022-02-27 15:49:24 +0300

    комментарий

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.shortcuts import render
 
 
 from .models import Post
@@ -9,9 +11,13 @@ LIMIT_POST = 10
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.select_related('group').all()[:LIMIT_POST]
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
 
