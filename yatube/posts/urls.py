df3b61f62b1f4cb8c3202ec19a95b106f35823a9
Любовь Беркова <grandprinceess7777@gmail.com>
2022-03-01 13:20:59 +0300

    комментарий

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,4 +7,9 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('group/<slug:slug>/', views.group_list, name='group_list'),
+    # Профайл пользователя
+    path('profile/<str:username>/', views.profile, name='profile'),
+    # Просмотр записи
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
+
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-from django.shortcuts import render
 
 
-from .models import Post
-from .models import Group
+from .models import Post, Group, User
 
 LIMIT_POST = 10
 
@@ -29,5 +27,28 @@ def group_list(request, slug):
     context = {
         'group': group,
         'posts': posts,
+    }
+    return render(request, template, context)
+
+
+def profile(request, username):
+    template = 'posts/profile.html'
+    user_db = get_object_or_404(User, username=username)
+    posts = user_db.posts.all()
+    posts_count = posts.count
+    page_obj = Paginator(posts, 10)
+    context = {
+        'posts_count': posts_count,
+        'user_db': user_db,
+        'page_obj': page_obj,
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+    template = 'posts/post_detail.html'
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'post': post
     }
     return render(request, template, context)
