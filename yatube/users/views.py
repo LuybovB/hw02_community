6cec6f8e8bbd869e1a58bac18545404786a141d1
Любовь Беркова <grandprinceess7777@gmail.com>
2022-03-02 14:16:49 +0300

    комментарий

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -5,5 +5,6 @@ from .forms import CreationForm
 
 class SignUp(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy("signup")
-    template_name = "signup.html"
+    # После успешной регистрации перенаправляем пользователя на главную.
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
